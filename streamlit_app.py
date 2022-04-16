5384b29113565ad2800c6080066003374c3b266d
Spencer <zestsx@gmail.com>
2022-04-16 22:08:22 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,7 +88,11 @@ def txt5(a, b):
 
 #####################
 # SideBar
+st.sidebar.markdown("**Links**")
 
+st.sidebar.markdown("Linkedin [Linkedin](https://www.linkedin.com/in/chew-shi-xiang-spencer-4a9a0582/)")
+st.sidebar.markdown("Tableau [Tableau](https://public.tableau.com/app/profile/spencer6275)!")
+st.sidebar.markdown("Linkedin [GitHub](https://github.com/Zestsx)!")
 
 #####################
 st.markdown('''
