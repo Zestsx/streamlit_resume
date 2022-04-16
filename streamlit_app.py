d188a6e79dbe67b47b79759e0e8701d17447b733
Spencer <zestsx@gmail.com>
2022-04-16 21:52:31 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,9 +26,13 @@ Second_Job_Date = datetime.date(2017, 12, 18)
 TWEP = abs(today- First_Job_Date).total_seconds()    
 PPWEP = abs(today- Second_Job_Date).total_seconds()  
 
-TWEP = divmod(TWEP, 31536000)[1]
+TWEP = divmod(TWEP, 31536000)[0]
 
-PPWEP = divmod(PPWEP, 31536000)[1]
+print(TWEP)
+
+PPWEP = divmod(PPWEP, 31536000)[0]
+
+print(PPWEP)
 
 
 
